77d9737206c0791ccfeb04eca7a5216f4d06fdeb
MarianaCidade <79975190+MarianaCidade@users.noreply.github.com>
2021-03-22 22:05:48 +0000

    Initial commit

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if(Random.value < 0.5f)
+        // {
+        //     GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
+        // }else
+        // {
+        //     GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.left;
+        // }
+
+        StartCoroutine(LancamentoBola());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+    }
+
+    IEnumerator LancamentoBola()
+    {
+        yield return new WaitForSeconds(2);
+
+        GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitCircle;
+    }
+}
---
+++ Assets/Script/Paddle.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 0.5f;
+
+    [SerializeField]
+    float paddleHeight = 1f;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+    // Update is called once per frame
+    void Update()
+    {
+       /*
+        * se tecla para cima premida
+        * então sbr
+        * se tecla para baixo premida
+        * então descer
+        */
+
+        /*
+         * Mover a raquete
+         */
+
+        if(Input.GetKey(upKey))
+
+        {
+            //Sobe
+            /*transform.position =
+             *  transform.position + velocidade * Vector3.up;
+             *  O QUE ESTÁ EM BAIXO É IGUAL A ISTO
+             */
+
+            transform.position += velocidade * Vector3.up * Time.deltaTime;
+
+        } else if (Input.GetKey(downKey))
+        {
+
+            //Desce
+            //transform.position =
+            //    transform.position + velocidade * Vector3.down;
+
+            transform.position += velocidade * Vector3.down * Time.deltaTime;
+        }
+
+
+        /*
+         * Verificar os limites (com ifs)
+         */
+
+        //float cameraHeight = Camera.main.orthographicSize;
+
+        //if (transform.position.y >= cameraHeight - 0.5f)
+        //{
+        //   //Variável auxiliar para poder alterar o valor de .y no transform.position
+        //
+        //    Vector3 positionAux = transform.position;
+        //    positionAux.y = cameraHeight - 0.5f;
+        //    transform.position = positionAux;
+        //
+        //}
+        //if (transform.position.y <= -cameraHeight + 0.5f)
+        //{
+        //    Vector3 positionAux = transform.position;
+        //    positionAux.y = -cameraHeight + 0.5f;
+        //    transform.position = positionAux;
+        //}
+
+
+        /*
+         * Verificar os limites (Clamp)
+         */
+
+
+        float cameraHeight = Camera.main.orthographicSize;
+        Vector3 positionAux = transform.position;
+        positionAux.y = Mathf.Clamp(positionAux.y, -cameraHeight + (paddleHeight * 0.5f), cameraHeight - (paddleHeight * 0.5f));
+        transform.position = positionAux;
+
+
+    }
+}
